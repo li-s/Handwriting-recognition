84195e3c0e08bd9b48fb4f55373403d1330300ce
lishuo <lishuo98@yahoo.com.sg>
2017-02-14 16:46:52 +0800

    Finished data_convert.py

--- data_convert.py
+++ data_convert.py
@@ -1,39 +1,31 @@
 import numpy as np
+import pickle
 from pprint import pprint
+from time import time
+
+import skimage.io as skio
+import matplotlib.pyplot as plt
 
 def convert():
-	with open('./data/training_image(binary).txt', 'w') as w:
+	with open('./data/training_image.pkl', 'wb') as w:
 		with open('/home/li/Downloads/train.csv', 'r') as read:
-			doc = []
-			a = ''.join(read)
-			b = a.split('\n')
-			for line in b:
-				c = line.split(',')
-				doc.append(c)	#doc represents the entire train.csv
-			doc2 = []
-			for i in range(1, len(doc)):
-				doc2.append(doc[i])	#doc2 represents the entire train.csv with each line as a seperate list
+			data = []
+			for line in read:
+				doc = line.split(',')
+				if doc[0] == 'Id':
+					continue
+				c = [int(ele) for ele in doc[4:]]
+				x = np.asarray(c, dtype = np.bool)
+				x = x.reshape((16, 8))
+				y = doc[1]
+				data.append((x, y))
 			
-			matrix = np.zeros( (16, 8) )
-			x = 0
-			x_max = 7
-			for i in range(0, len(doc)):	#literates over the number of training images
-				y = 0
-				for j in range(4, len(doc[0])):	#only literates over the binary part
-					if x < x_max:	#
-						matrix[y][x] = doc2[0][j]	# substitude the matrix for the binary
-						x += 1
-					else:
-						print(x, y)
-						y += 1
-						x = 0
-					
-			print(len(doc))
-			pprint(matrix, stream = w)
-			
-'''
-	line 20: need to find way to initialise multiple numpy arrays at the same time(number of arrays = len(doc2)-1, as the first line is not important to us right now)
-'''					
 
+			for i in range(15):
+				skio.imshow(data[i][0])
+				print(data[i][1])
+				plt.show()
 
+start = time()
 convert()
+print('The program ran for: {}'.format(time() - start))
--- test.py
+++ test.py
@@ -1,19 +1,11 @@
-import numpy
-from pprint import pprint
+import skimage.io as skio
+import matplotlib.pyplot as plt
+import pickle
 
-def convert():
-	with open('./data/training_image(binary).txt', 'w') as w:
-		with open('/home/li/Downloads/train.csv', 'r') as read:
-			doc = []
-			a = ''.join(read)
-			b = a.split('\n')
-			for line in b:
-				c = line.split(',')
-				doc.append(c)
-#			
-#			for i in range(1, len(b)):
-#				doc.append([b[i]])
-#			
-			pprint(doc, stream = w)
-
-convert()
+with open('./data/training_image.pkl', 'rb') as read:
+	a = pickle.load(read)
+	print(a.shape)
+#	img = skio.imread()
+	skio.imshow(a)
+	plt.show()
+	
