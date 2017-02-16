ae0ad9b06ea0d119bab3a92a94b7e203610b2997
li shuo <lishuo98@yahoo.com.sg>
2017-02-16 19:51:02 +0800

    Update model.py

--- model.py
+++ model.py
@@ -1,12 +1,45 @@
 from keras.models import Sequential
 from keras.layers import Dense
+from keras.layers import Dropout
+from keras.layers import Flatten
+from keras.layers.convolutional import Convolution2D
+from keras.layers.convolutional import MaxPooling2D
+from keras import backend as K
 
-def baseline_model(num_pixels, num_classes):
-	# create model
+K.set_image_dim_ordering('th')
+
+# define baseline model
+def baseline_model():
 	model = Sequential()
 	model.add(Dense(num_pixels, input_dim=num_pixels, init='normal', activation='relu'))
 	model.add(Dense(num_classes, init='normal', activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+    
+def simple_CNN_model():
+	model = Sequential()
+	model.add(Convolution2D(32, 5, 5, border_mode='valid', input_shape=(1, 28, 28), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.2))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu'))
+	model.add(Dense(num_classes, activation='softmax'))
+	# Compile model
+	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
 
+def larger_CNN_model():
+	model = Sequential()
+	model.add(Convolution2D(30, 5, 5, border_mode='valid', input_shape=(1, 28, 28), activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Convolution2D(15, 3, 3, activation='relu'))
+	model.add(MaxPooling2D(pool_size=(2, 2)))
+	model.add(Dropout(0.2))
+	model.add(Flatten())
+	model.add(Dense(128, activation='relu'))
+	model.add(Dense(50, activation='relu'))
+	model.add(Dense(num_classes, activation='softmax'))
 	# Compile model
 	model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 	return model
