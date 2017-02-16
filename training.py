fde07f2354e984610f0dab613f8195c59f10eb39
li shuo <lishuo98@yahoo.com.sg>
2017-02-16 20:16:15 +0800

    changed test to val

--- training.py
+++ training.py
@@ -7,32 +7,32 @@ import json
 
 seed = 7
 numpy.random.seed(seed)
-#(x_train, y_train), (x_test, y_test) = mnist.load_data()
+#(x_train, y_train), (x_val, y_val) = mnist.load_data()
 
 
-def training(x_train, y_train, x_test, y_test):
+def training(x_train, y_train, x_val, y_val):
 	'''
 	format of mnist:
-	((([x_train], dtype = unit8), ([y_train])), (([x_test]), dtype = unit8), ([y_test])))
+	((([x_train], dtype = unit8), ([y_train])), (([x_val]), dtype = unit8), ([y_val])))
 	'''
 	# flatten 28*28 images to a 784 vector for each image
 	num_pixels = x_train.shape[1] * x_train.shape[2]
 	x_train = x_train.reshape(x_train.shape[0], num_pixels).astype('float32')
-	x_test = x_test.reshape(x_test.shape[0], num_pixels).astype('float32')
+	x_val = x_val.reshape(x_val.shape[0], num_pixels).astype('float32')
 
 	#one hot encoding -> converts the 26 alphabets(represented as integers) to a categorical system where the machine understands
 	y_train = np_utils.to_categorical(y_train)
-	y_test = np_utils.to_categorical(y_test)
-	num_classes = y_test.shape[1]
+	y_val = np_utils.to_categorical(y_val)
+	num_classes = y_val.shape[1]
 
 	# build the model
 	model = baseline_model(num_pixels, num_classes)
 
 	# Fit the model
-	model.fit(x_train, y_train, validation_data=(x_test, y_test), nb_epoch=10, batch_size=200, verbose=2)
+	model.fit(x_train, y_train, validation_data=(x_val, y_val), nb_epoch=10, batch_size=200, verbose=2)
 
 	# Final evaluation of the model
-	scores = model.evaluate(x_test, y_test, verbose=0)
+	scores = model.evaluate(x_val, y_val, verbose=0)
 
 #	model.summary()
 
@@ -43,11 +43,11 @@ if __name__ == '__main__':
 		mydataset = pickle.load(a)
 		(x_train, y_train) = mydataset
 
-	with open('./data/test_image.pkl', 'rb') as a:
+	with open('./data/val_image.pkl', 'rb') as a:
 		mydataset = pickle.load(a)
-		(x_test, y_test) = mydataset
+		(x_val, y_val) = mydataset
 
-	scores, model = training(x_train, y_train, x_test, y_test)
+	scores, model = training(x_train, y_train, x_val, y_val)
 
 	#saves model weights
 	a = input('Model name: ')
