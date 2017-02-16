6d13b303ba7154e3e52c5efd59f2974984bdccd5
li shuo <lishuo98@yahoo.com.sg>
2017-02-16 21:02:41 +0800

    Added option to initialize a test image set

--- utils/data_convert.py
+++ utils/data_convert.py
@@ -4,7 +4,7 @@ from pprint import pprint
 from time import time
 from random import sample
 
-def convert_train():
+def convert_train(option):
 	with open('/home/li/Downloads/train.csv', 'r') as read:
 		#makes the training sample for keras
 		array = []
@@ -27,21 +27,32 @@ def convert_train():
 			a = ord(i) - ord('a')
 			answer.append(int(a))
 
-		#selects 1000 images for testing out of the 40,000++ train images and removes it from the array
-		train_array = []
-		train_answer = []
-		val_array = []
-		val_answer = []
-		a = sample(range(len(array)), 1000)
-		for i in range(len(array)):
-			if i in a:
-				val_array.append(array[i])
-				val_answer.append(answer[i])
+        if option == y:
+    		#selects 1000 images for testing out of the 40,000++ train images and removes it from the array
+    		train_array = []
+    		train_answer = []
+    		val_array = []
+    		val_answer = []
+    		a = sample(range(len(array)), 1000)
+    		for i in range(len(array)):
+    			if i in a:
+    				val_array.append(array[i])
+    				val_answer.append(answer[i])
 
-			else:
-				train_array.append(array[i])
-				train_answer.append(answer[i])
+    			else:
+    				train_array.append(array[i])
+    				train_answer.append(answer[i])
+        	#format for val
+            val = []
+            val_array = np.asarray(val_array, dtype = np.int)
+            val_answer = np.asarray(val_answer, dtype = np.int)
+            val.append(val_array)
+            val.append(val_answer)
 
+        elif int(option) == n:
+            train_array = array
+    		train_answer = answer
+            val = None
 
 		#format for train
 		train = []
@@ -50,18 +61,12 @@ def convert_train():
 		train.append(train_array)
 		train.append(train_answer)
 
-		#format for val
-		val = []
-		val_array = np.asarray(val_array, dtype = np.int)
-		val_answer = np.asarray(val_answer, dtype = np.int)
-		val.append(val_array)
-		val.append(val_answer)
-
 		return train, val
 
 if __name__ == '__main__':
 	start = time()
-	train, val = convert_train()
+    option = input('Do you want to initiate testing images(y/n)?\n')
+	train, val = convert_train(option)
 	with open('../data/train_image.pkl', 'wb') as w:
 		pickle.dump(train, w)
 	with open('../data/val_image.pkl', 'wb') as w:
